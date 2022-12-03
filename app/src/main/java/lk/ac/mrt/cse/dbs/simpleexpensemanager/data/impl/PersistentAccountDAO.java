b1c86a1dd6ee7beb980fcc805b370e3251cec118
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2022-12-03 12:16:43 +0530

    Add implementation for getAccount(), addAccount(), removeAccount() and updateBalance() in PersistentAccountDAO

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
@@ -1,6 +1,7 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -73,21 +74,74 @@ public class PersistentAccountDAO implements AccountDAO {
 
     @Override
     public Account getAccount(String accountNo) throws InvalidAccountException {
-        return null;
+        Account account = null;
+
+        // Prepare query to fetch a specific accounts
+        String[] columns = new String[]{DatabaseHelper.ACCOUNT_NO, DatabaseHelper.BANK_NAME, DatabaseHelper.ACCOUNT_HOLDER_NAME, DatabaseHelper.BALANCE};
+        Cursor cursor = db.query(DatabaseHelper.ACCOUNT_TABLE, columns, DatabaseHelper.ACCOUNT_NO + " = ?", new String[]{accountNo}, null, null, null);
+
+        // Check if account exists in db
+        if (cursor != null) {
+            cursor.moveToFirst();
+
+            // Create account object
+            account = new Account(accountNo,
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.BANK_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.ACCOUNT_HOLDER_NAME)),
+                    cursor.getDouble(cursor.getColumnIndexOrThrow(DatabaseHelper.BALANCE)));
+        } else {
+            String msg = "Account " + accountNo + " is invalid.";
+            throw new InvalidAccountException(msg);
+        }
+
+        return account;
     }
 
     @Override
     public void addAccount(Account account) {
+        ContentValues contentValues = new ContentValues();
 
+        // Store values to be inserted in contentValues
+        contentValues.put(DatabaseHelper.ACCOUNT_NO, account.getAccountNo());
+        contentValues.put(DatabaseHelper.BANK_NAME, account.getBankName());
+        contentValues.put(DatabaseHelper.ACCOUNT_HOLDER_NAME, account.getAccountHolderName());
+        contentValues.put(DatabaseHelper.BALANCE, account.getBalance());
+
+        // Insert account into db
+        db.insert(DatabaseHelper.ACCOUNT_TABLE, null, contentValues);
     }
 
     @Override
     public void removeAccount(String accountNo) throws InvalidAccountException {
-
+        // Check if account exists and delete it from db
+        Account account = getAccount(accountNo);
+        if (account != null) {
+            db.delete(DatabaseHelper.ACCOUNT_TABLE, DatabaseHelper.ACCOUNT_NO + " = ?", new String[]{accountNo});
+        }
     }
 
     @Override
     public void updateBalance(String accountNo, ExpenseType expenseType, double amount) throws InvalidAccountException {
+        // Check if account exists and fetch it from db
+        Account account = getAccount(accountNo);
+        if (account != null) {
+            double currentBalance = account.getBalance();
+            double finalBalance = currentBalance;
 
+            // Specific implementation based on the transaction type
+            switch (expenseType) {
+                case EXPENSE:
+                    finalBalance = currentBalance - amount;
+                    break;
+                case INCOME:
+                    finalBalance = currentBalance + amount;
+                    break;
+            }
+
+            // Update balance for account in db
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(DatabaseHelper.BALANCE, finalBalance);
+            db.update(DatabaseHelper.ACCOUNT_TABLE, contentValues, DatabaseHelper.ACCOUNT_NO + " = ?", new String[]{accountNo});
+        }
     }
 }
